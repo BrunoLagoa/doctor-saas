2d2ce36bb5459949b88ed62b8045d8980873d29b
Bruno Castro <miroldols@gmail.com>
2025-06-10 09:29:56 -0300

    feat(patients): integrate @tanstack/react-table for patient data management and create DataTable component

--- src/app/(protected)/patients/page.tsx
+++ src/app/(protected)/patients/page.tsx
@@ -2,6 +2,7 @@ import { asc, eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { DataTable } from "@/components/ui/data-table";
 import {
   PageContainer,
   PageContent,
@@ -16,7 +17,7 @@ import { patientsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import AddPatientButton from "./_components/add-patient-button";
-import PatientCard from "./_components/patient-card";
+import { patientsTableColumns } from "./_components/table-columns";
 
 export default async function PatientsPage() {
   const session = await auth.api.getSession({
@@ -50,11 +51,12 @@ export default async function PatientsPage() {
         </PageHeaderActions>
       </PageHeader>
       <PageContent>
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+        {/* <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
           {patients.map((patient) => (
             <PatientCard key={patient.id} patient={patient} />
           ))}
-        </div>
+        </div> */}
+        <DataTable columns={patientsTableColumns} data={patients} />
       </PageContent>
     </PageContainer>
   );
